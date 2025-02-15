65279a6b52918a6e964506e32569f06b3c0a1270
ChrisAtUW <146300146+ChrisAtUW@users.noreply.github.com>
2025-02-14 19:51:18 -0800

    Add files via upload
    Added functions to detect free fall, read peak acceleration, and calculate deacceleration time (might be broken).

---
+++ Metrom4AccelDropTesting.ino
@@ -0,0 +1,301 @@
+// Adafruit SPI Flash FatFs Simple Datalogging Example
+// Author: Tony DiCola
+//
+// This is a simple dataloging example using the SPI Flash
+// FatFs library.  The example will open a file on the SPI
+// flash and append new lines of data every minute. Note that
+// you MUST have a flash chip that's formatted with a flash
+// filesystem before running.  See the fatfs_format example
+// to perform this formatting.
+//
+// Usage:
+// - Modify the pins and type of fatfs object in the config
+//   section below if necessary (usually not necessary).
+// - Upload this sketch to your M0 express board.
+// - Open the serial monitor at 115200 baud.  You should see the
+//   example print a message every minute when it writes a new
+//   value to the data logging file.
+//Above is Usage Instructions
+
+//NOTICE: You need to format the storage for a brand new m4 that hasn't been setup. Run the format example in the Adafruit SPIFlash Library
+
+#include <SPI.h>
+#include <SdFat.h>
+
+#include <Adafruit_SPIFlash.h>
+
+// for flashTransport definition
+#include "flash_config.h"
+Adafruit_SPIFlash flash(&flashTransport);
+
+//for Adafruit Accel/Gyro
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+Adafruit_MPU6050 mpu;
+
+
+// file system object from SdFat
+FatVolume fatfs;
+
+// Configuration for the datalogging file:
+#define FILE_NAME "data.csv"
+
+float dropAccelThresh = 30.0;
+float freeFallThresh = 2.0;
+float restingAccel = 9.0;
+
+void setup() {
+  // Initialize serial port and wait for it to open before continuing.
+  Serial.begin(115200);
+  while (!Serial) {
+    delay(100);
+  }
+  Serial.println("Adafruit SPI Flash FatFs Data Logging Hardware Info:");
+
+  // Initialize flash library and check its chip ID.
+  if (!flash.begin()) {
+    Serial.println("Error, failed to initialize flash chip!");
+    while (1) {
+      delay(1);
+    }
+  }
+  Serial.print("Flash chip JEDEC ID: 0x");
+  Serial.println(flash.getJEDECID(), HEX);
+
+  // First call begin to mount the filesystem.  Check that it returns true
+  // to make sure the filesystem was mounted.
+  if (!fatfs.begin(&flash)) {
+    Serial.println("Error, failed to mount newly formatted filesystem!");
+    Serial.println(
+      "Was the flash chip formatted with the fatfs_format example?");
+    while (1) {
+      delay(1);
+    }
+  }
+  Serial.println("Mounted filesystem!");
+  //Serial.println("Logging data every 60 seconds...");
+  Serial.println("---------------------------------------");
+  Serial.println();
+
+    //Accelerometer Sensor
+    Serial.println("Setting up Accelerometer/Gyro");
+    if (!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1) {
+      delay(10);
+    }
+  }
+  Serial.println("MPU6050 Found!");
+
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+  Serial.print("Accelerometer range set to: ");
+  switch (mpu.getAccelerometerRange()) {
+    case MPU6050_RANGE_2_G:
+      Serial.println("+-2G");
+      break;
+    case MPU6050_RANGE_4_G:
+      Serial.println("+-4G");
+      break;
+    case MPU6050_RANGE_8_G:
+      Serial.println("+-8G");
+      break;
+    case MPU6050_RANGE_16_G:
+      Serial.println("+-16G");
+      break;
+  }
+  mpu.setGyroRange(MPU6050_RANGE_2000_DEG);
+  Serial.print("Gyro range set to: ");
+  switch (mpu.getGyroRange()) {
+    case MPU6050_RANGE_250_DEG:
+      Serial.println("+- 250 deg/s");
+      break;
+    case MPU6050_RANGE_500_DEG:
+      Serial.println("+- 500 deg/s");
+      break;
+    case MPU6050_RANGE_1000_DEG:
+      Serial.println("+- 1000 deg/s");
+      break;
+    case MPU6050_RANGE_2000_DEG:
+      Serial.println("+- 2000 deg/s");
+      break;
+  }
+
+  mpu.setFilterBandwidth(MPU6050_BAND_260_HZ);
+  Serial.print("Filter bandwidth set to: ");
+  switch (mpu.getFilterBandwidth()) {
+    case MPU6050_BAND_260_HZ:
+      Serial.println("260 Hz");
+      break;
+    case MPU6050_BAND_184_HZ:
+      Serial.println("184 Hz");
+      break;
+    case MPU6050_BAND_94_HZ:
+      Serial.println("94 Hz");
+      break;
+    case MPU6050_BAND_44_HZ:
+      Serial.println("44 Hz");
+      break;
+    case MPU6050_BAND_21_HZ:
+      Serial.println("21 Hz");
+      break;
+    case MPU6050_BAND_10_HZ:
+      Serial.println("10 Hz");
+      break;
+    case MPU6050_BAND_5_HZ:
+      Serial.println("5 Hz");
+      break;
+  }
+
+  Serial.println("Accelerometer Setup Complete");
+  Serial.println("--------------------------------------");
+  Serial.println();
+
+  delay(100);
+}
+
+void logSensorData() {
+  Serial.println("Collecting and Storing Accelerometer/Gyro Data...");
+  // Open the datalogging file for writing.  The FILE_WRITE mode will open
+    // the file for appending, i.e. it will add new data to the end of the file.
+    File32 dataFile = fatfs.open(FILE_NAME, FILE_WRITE);
+    // Check that the file opened successfully and write a line to it.
+    if (dataFile) {
+      // Take a new data reading from a Accel
+      /* Get new sensor events with the readings */
+      sensors_event_t a, g, temp;
+      mpu.getEvent(&a, &g, &temp);
+
+      // Write a line to the file.  You can use all the same print functions
+      // as if you're writing to the serial monitor.  For example to write
+      // two CSV (commas separated) values:
+
+      dataFile.print("Data Log #");
+      dataFile.println("Sensor #1:");
+      dataFile.println("Acceleration X: ");
+      dataFile.print(a.acceleration.x);
+      dataFile.print(", Y: ");
+      dataFile.print(a.acceleration.y);
+      dataFile.print(", Z: ");
+      dataFile.print(a.acceleration.z);
+      dataFile.println(" m/s^2");
+
+      dataFile.print("Rotation X: ");
+      dataFile.print(g.gyro.x);
+      dataFile.print(", Y: ");
+      dataFile.print(g.gyro.y);
+      dataFile.print(", Z: ");
+      dataFile.print(g.gyro.z);
+      dataFile.println(" rad/s");
+
+      dataFile.print("Temperature: ");
+      dataFile.print(temp.temperature);
+      dataFile.println(" degC");
+
+      dataFile.println("");
+      // Finally close the file when done writing.  This is smart to do to make
+      // sure all the data is written to the file.
+      dataFile.close();
+    } else {
+      Serial.println("Failed to open data file for writing!");
+    }
+  
+  Serial.println("Wrote measurements to data file!");
+  Serial.println("Collection and Storage Complete");
+  Serial.println("---------------------------------");
+  Serial.println();
+}
+
+void readMetroData() {
+  Serial.println("Reading from SPI Flash Storage...");
+  
+  //Read data from SPI Flash
+  Serial.println("data stored in SPI Flash:");
+  File32 myFile = fatfs.open(FILE_NAME);
+  if (myFile) {
+    // read from the file until there's nothing else in it:
+    while (myFile.available()) {
+      Serial.write(myFile.read());
+    }
+    // close the file:
+    myFile.close();
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening ");
+    Serial.print(FILE_NAME);
+    Serial.print(".txt");
+  }
+
+  delay(1000);
+  Serial.println("Read Complete");
+  Serial.println("--------------------------");
+  Serial.println();
+}
+
+void drop() {
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+
+  float xAcel = a.acceleration.x;
+  float yAcel = a.acceleration.y;
+  float zAcel = a.acceleration.z;
+
+  float totalAccel1 = sqrt((xAcel*xAcel) + (yAcel*yAcel) + (zAcel*zAcel));
+  Serial.print("Total Acceleration:"); //dont put spaces serial plotter doesnt recognize
+  Serial.print(totalAccel1);
+  Serial.println();
+
+  bool steadyState = false;
+  int impact = 0;
+  float peakAccel = 0.0;
+  unsigned long impactStart = 0;
+  unsigned long impactEnd = 0;
+  unsigned long impactDuration = 0;
+  
+  //if reach free fall start logging
+  if (totalAccel1 < freeFallThresh) {
+    while (!steadyState) { 
+      //once reach free fall log until reach equilibrium, to reach equilibrium must wait for spike
+      sensors_event_t a, g, temp;
+      mpu.getEvent(&a, &g, &temp);
+
+      float xAcel = a.acceleration.x;
+      float yAcel = a.acceleration.y;
+      float zAcel = a.acceleration.z;
+
+      float totalAccel = sqrt((xAcel*xAcel) + (yAcel*yAcel) + (zAcel*zAcel));
+      
+      Serial.print("Total Acceleration:"); //dont put spaces serial plotter doesnt recognize
+      Serial.print(totalAccel);
+      Serial.println();
+
+      float temp1 = 0.0;
+
+      if (totalAccel > dropAccelThresh) {
+        temp1 = totalAccel; //this prevents wrong peak accel
+        if (temp1 >= peakAccel) {
+          peakAccel = temp1; //return peak accel
+          impactStart = millis();
+          impact +=1;
+        }  
+      }
+
+      if ((impact > 0) && (totalAccel < 8.9)) { //impact detected and at rest i.e steady state
+        impactEnd = millis();
+        impactDuration = impactEnd - impactStart; //return impact duration
+        steadyState = true;
+      }
+    }
+    Serial.println("Completed");
+    Serial.println("MaxAccel:");
+    Serial.println(peakAccel);
+    Serial.println("impactduration");
+    Serial.println(impactDuration);
+    delay(10000);
+  }
+
+}
+
+void loop() {
+  drop();
+}
